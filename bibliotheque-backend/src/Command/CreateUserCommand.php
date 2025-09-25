f143d6da63246dee24957467756ba6a815df9f9f
Dimitri <dev@perriet.fr>
2025-09-25 23:15:21 +0200

    Ajout de l'authentification JWT avec le bundle lexik/jwt-authentication-bundle, création de l'entité User, d'une commande pour créer des utilisateurs, et mise à jour des fichiers de configuration pour gérer la sécurité et les routes.

---
+++ bibliotheque-backend/src/Command/CreateUserCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-user',
+    description: 'Créer un nouvel utilisateur',
+)]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'Email de l\'utilisateur')
+            ->addArgument('password', InputArgument::REQUIRED, 'Mot de passe')
+            ->addArgument('nom', InputArgument::REQUIRED, 'Nom de famille')
+            ->addArgument('prenom', InputArgument::REQUIRED, 'Prénom')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+        $nom = $input->getArgument('nom');
+        $prenom = $input->getArgument('prenom');
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setNom($nom);
+        $user->setPrenom($prenom);
+        
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $io->success(sprintf('Utilisateur %s créé avec succès !', $email));
+
+        return Command::SUCCESS;
+    }
+}
