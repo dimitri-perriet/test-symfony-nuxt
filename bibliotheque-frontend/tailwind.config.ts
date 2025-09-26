f53dc9d006f1ffa324864aa915a0c9e468a58f4b
dimitri <dev@perriet.fr>
2025-09-26 09:57:34 +0200

    Initialisation du front-end avec Nuxt, DaisyUI et Tailwind

---
+++ bibliotheque-frontend/tailwind.config.ts
@@ -0,0 +1,44 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './nuxt.config.{js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula'
+    ],
+  },
+} satisfies Config
+
